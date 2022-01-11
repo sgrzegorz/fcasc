1b7a6c59780ffe040312c7d98e2b17568e202f42
Jan Rodzon <rodzonjan@wp.pl>
2022-01-12 00:21:49 +0100

    Add removing port forwarding

--- README.md
+++ README.md
@@ -12,13 +12,19 @@ To setup cluster:
 To check if cluster is deployed see:
 ```
 helm list
+```
+```
 kubectl get pods
+```
+```
 kubectl get services
 ```
 
-To uninstall minio:
+To uninstall resources:
 ```
-helm uninstall $(helm list --filter 'minio' | awk '{print $1}' | tail -n 1)
+MINIO_HELM_DEPLOYMENT=$(helm list --filter=minio | awk '{print $1}' | tail -n 1)
+helm uninstall $MINIO_HELM_DEPLOYMENT
+./remove_port_forwarding.sh
 ```
 
 To clear all kubernetes cluster resources:
---
+++ remove_port_forwarding.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: $0 [-a]"
+    echo "Without arguments remove port forwarding only from the current minio deployment."
+    echo "Arguments:"
+    echo "-a, remove all minio port forwardings"
+}
+
+MINIO_HELM_DEPLOYMENT=$(helm list --filter=minio | awk '{print $1}' | tail -n 1)
+
+while getopts "ah" o; do
+    case "${o}" in
+        a)
+            MINIO_HELM_DEPLOYMENT="minio"
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            ;;
+    esac
+done
+
+while (($(ps aux | grep "$MINIO_HELM_DEPLOYMENT" | grep "port-forward" | wc -l) > 1 )); do
+  PID_TO_REMOVE=$(ps aux | grep "$MINIO_HELM_DEPLOYMENT" | grep "port-forward" | awk '{print $2}' | head -n 1)
+  echo "Removing port forwarding of $MINIO_HELM_DEPLOYMENT with pid $PID_TO_REMOVE"
+  kill -9 $PID_TO_REMOVE
+done
+
